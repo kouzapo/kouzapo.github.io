d545846d810864fe972c18ca4fc67bf4ec63dafc
kouzapo <kouzoukos97@gmail.com>
2020-01-30 14:48:16 +0200

    aaa

---
+++ upgrade_packages.bat
@@ -0,0 +1,20 @@
+@echo off
+
+pip install -U scipy
+pip install -U numpy
+pip install -U sympy
+pip install -U pandas
+pip install -U pandas-datareader
+pip install -U scikit-learn
+pip install -U matplotlib
+pip install -U keras
+pip install -U theano
+pip install -U flask
+pip install -U bs4
+pip install -U lxml
+pip install -U urllib3
+pip install -U requests
+pip install -U websockets
+pip install -U pika
+pip install -U dill
+pip install -U psutil
